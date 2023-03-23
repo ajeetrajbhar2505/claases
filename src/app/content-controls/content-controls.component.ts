96d8ddca1741a90fb398f764ccf47be310697cb6
ajeet rajbhar <ajeetrajbhar2504@gmail.com>
2023-03-24 02:19:33 +0530

    creating module that can navigate from any page to any content and back to previous

--- src/app/content-controls/content-controls.component.ts
+++ src/app/content-controls/content-controls.component.ts
@@ -63,6 +63,7 @@ export class ContentControlsComponent {
   ) {
     this.ActivatedRoute.queryParams.subscribe(async (param: any) => {
       this.contentDetails = param;
+      this.contentControls.openFullscreen = false
       let response: any = await this.http.get('assets/LecturesWiseVideos.json').toPromise()
       response.filter((Object:any)=>{
         if (Object.lec_id == param.lec_id) {
