8dd6c6418d63aaf7ec6ce8c9e33cbee62946f14f
ajeet rajbhar <ajeetrajbhar2504@gmail.com>
2023-03-24 16:51:58 +0530

    App Update : Beautified home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,28 @@ export class AppComponent {
   });
   }
 
+  private getContentElement(): HTMLVideoElement | null {
+    return document.querySelector<HTMLVideoElement>('#classContent');
+  }
+  private getliveContentElement(): HTMLVideoElement | null {
+    return document.querySelector<HTMLVideoElement>('#liveVideo');
+  }
 
     // Auto hide show loader
-    checkRouterEvent(routerEvent: Event): void {
+
+
+    
+    checkRouterEvent(routerEvent: any): void {
       if (routerEvent instanceof NavigationStart) {
+        if (this.getContentElement()) {
+          this.getContentElement()?.pause()
+        }
+        console.log(routerEvent.url); // This will log the current URL
+
+        
+        if (routerEvent.url !== '/tabs/live' && this.getliveContentElement()) {
+          this.getliveContentElement()?.pause()
+        }
       }
 
       if (routerEvent instanceof NavigationEnd ||
