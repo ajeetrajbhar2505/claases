c7e14527543d13ca08d92c56d6dcb68a309ba209
ajeet rajbhar <ajeetrajbhar2504@gmail.com>
2023-04-17 00:19:56 +0530

    App Update : Added speack to search

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeaturesComponent } from './features.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '',component : FeaturesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
