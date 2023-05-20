2cdfde4d9dfdfb5341e62cee818ad71512546326
ajeet rajbhar <ajeetrajbhar2504@gmail.com>
2023-05-21 01:14:39 +0530

    created upload excel functionality and added custom snackbar

--- src/app/upload-video/upload-video.component.ts
+++ src/app/upload-video/upload-video.component.ts
@@ -12,108 +12,111 @@ import { WebService } from '../web.service';
   styleUrls: ['./upload-video.component.scss'],
 })
 export class UploadVideoComponent implements OnInit {
-  lecturesData:any[] = []
-  classData:any = []
-  params:any = {}
+  lecturesData: any[] = []
+  classData: any = []
+  params: any = {}
   uploadVideogroup = new FormGroup({
-    classId : new FormControl(''),
-    lec_id : new FormControl(''),
-    content_icon : new FormControl(''),
-    content_link : new FormControl(''),
-    content_title : new FormControl(''),
-    contentId : new FormControl(''),
-    content : new FormControl('video'),
-    published_at : new FormControl(''),
-    content_file : new FormControl(''),
+    classId: new FormControl(''),
+    lec_id: new FormControl(''),
+    content_icon: new FormControl(''),
+    content_link: new FormControl(''),
+    content_title: new FormControl(''),
+    contentId: new FormControl(''),
+    content: new FormControl('video'),
+    published_at: new FormControl(''),
+    content_file: new FormControl(''),
   })
-  currentContent:any = ""
-  uploading:boolean = false
+  currentContent: any = ""
+  uploading: boolean = false
 
-  constructor(public http:HttpClient,private sanitizer: DomSanitizer,public router:Router,public service:WebService,public ActivatedRoute:ActivatedRoute) {
-   this.uploadVideogroup.get('published_at')?.patchValue(this.service.getCurrentDate())
-   ActivatedRoute.queryParams.subscribe((params:any)=>{
-    this.params = params
-   this.uploadVideogroup.get('content')?.patchValue(params.content)
-  })
+  constructor(public http: HttpClient, private sanitizer: DomSanitizer, public router: Router, public service: WebService, public ActivatedRoute: ActivatedRoute) {
+    this.uploadVideogroup.get('published_at')?.patchValue(this.service.getCurrentDate())
+    ActivatedRoute.queryParams.subscribe((params: any) => {
+      this.params = params
+      this.uploadVideogroup.get('content')?.patchValue(params.content)
+    })
   }
 
- async ngOnInit() {
-  this.classData = await this.http.get('assets/classWiseLectures.json').toPromise()
- }
+  async ngOnInit() {
+    this.classData = await this.http.get('assets/classWiseLectures.json').toPromise()
+  }
 
- readUrl(event: any) {
-  this.uploadVideogroup.get("content_file")?.patchValue(event.target.files[0]);
-  this.uploadVideogroup.get("content_link")?.patchValue(event.target.files[0].name);
-}
+  readUrl(event: any) {
+    this.uploadVideogroup.get("content_file")?.patchValue(event.target.files[0]);
+    this.uploadVideogroup.get("content_link")?.patchValue(event.target.files[0].name);
+  }
 
 
-getImgContent(url: any): SafeUrl {
-  return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-}
+  getImgContent(url: any): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 
- async getSubjectsByclassId(event:any)
- {
-  let classId = event.target.value
-    await this.http.get('assets/classWiseLectures.json').toPromise().then((response:any)=>{   
-    response.filter((data:any) =>  { 
-      if (data.classId == classId) {
-        this.lecturesData = data['subjects']
-      }
+  async getSubjectsByclassId(event: any) {
+    let classId = event.target.value
+    await this.http.get('assets/classWiseLectures.json').toPromise().then((response: any) => {
+      response.filter((data: any) => {
+        if (data.classId == classId) {
+          this.lecturesData = data['subjects']
+        }
       })
-      
-  })
- }
 
- switchContent(content:any)
- {
-   this.currentContent = content
-   this.uploadVideogroup.get('content')?.patchValue(content)
- }
- 
- onchange_lecture(event:any)
- {
- 
-  this.lecturesData.filter((data:any) =>  { 
-    if (data.lec_id ==  this.uploadVideogroup.get('lec_id')?.value) {
-      this.uploadVideogroup.get('content_icon')?.patchValue(data.lec_icon)
-    }})
- }
-
- backToContent()
- {
-  const queryParams = {
-    classId: this.params.classId,
-    lec_id: this.params.lec_id,
-    contentId: this.params.contentId,
-    from : '/tabs/lectures'
-  };
-  this.router.navigate([this.params.from], { queryParams });
- }
-
-async uploadContent()
- {
-
-  this.uploading = true
-  let body:any = {...this.uploadVideogroup.value}
-
-// create a new FormData object
-const formData = new FormData();
-for (const key in body) {
-  if (body.hasOwnProperty(key)) {
-    formData.append(key, body[key]);
+    })
   }
-}
 
-  // formData can now be sent in an HTTP request
-  // try {
-  //   let response:any = await this.http.post(environment.nodeApi + '/uploadVideo',formData).toPromise()
-  //   if (response.status == 204) {
-  //    this.uploading = false
-  //   }
-  // } catch (error) {
-  //   this.uploading = false
-  // }
+  switchContent(content: any) {
+    this.currentContent = content
+    this.uploadVideogroup.get('content')?.patchValue(content)
+  }
 
- }
+  onchange_lecture(event: any) {
+
+    this.lecturesData.filter((data: any) => {
+      if (data.lec_id == this.uploadVideogroup.get('lec_id')?.value) {
+        this.uploadVideogroup.get('content_icon')?.patchValue(data.lec_icon)
+      }
+    })
+  }
+
+  backToContent() {
+    const queryParams = {
+      classId: this.params.classId,
+      lec_id: this.params.lec_id,
+      contentId: this.params.contentId,
+      from: '/tabs/lectures'
+    };
+    this.router.navigate([this.params.from], { queryParams });
+  }
+
+  async uploadContent() {
+
+    this.uploading = true
+    let body: any = { ...this.uploadVideogroup.value }
+
+    // create a new FormData object
+    const formData = new FormData();
+    for (const key in body) {
+      if (body.hasOwnProperty(key)) {
+        formData.append(key, body[key]);
+      }
+    }
+
+    // formData can now be sent in an HTTP request
+    // try {
+    //   let response:any = await this.http.post(environment.nodeApi + '/uploadVideo',formData).toPromise()
+    //   if (response.status == 204) {
+    //    this.uploading = false
+    //   }
+    // } catch (error) {
+    //   this.uploading = false
+    // }
+
+  }
+
+  async uploadExcel(event: any) {
+   let uploadExcelResponse = await this.service.uploadExcelFile(event.target.files[0])
+   console.log(uploadExcelResponse);
+   
+  }
+
 
 }
--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,26 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { io } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
+import * as XLSX from 'xlsx';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
+  
 })
 export class WebService {
   private isLoggedIn = false;
   public isSplashLoaded = false;
+  public alertButtons = ['OK'];
+
 
 
   local = "http://192.168.31.159:3000"
-  socket:any
-  constructor(private router:Router) {
+  socket: any
+  constructor(private router: Router, public http: HttpClient) {
     this.socket = io(environment.nodeApi, {
       transports: ['websocket']
     });
-   }
+  }
 
 
-   
+
 
 
   speech(textToSpeech: string) {
@@ -29,7 +35,7 @@ export class WebService {
 
     // create a new SpeechSynthesisUtterance object with the desired text
     console.log(textToSpeech);
-    
+
     const utterance = new SpeechSynthesisUtterance(textToSpeech);
 
     // get all available voices
@@ -51,8 +57,8 @@ export class WebService {
       utterance.voice = maleVoice;
     }
 
-      utterance.voice = maleVoice;
-      utterance.lang = 'hi-IN';
+    utterance.voice = maleVoice;
+    utterance.lang = 'hi-IN';
 
 
     // queue the utterance for synthesis
@@ -91,38 +97,110 @@ export class WebService {
   }
 
 
-  getCurrentDate(){
+  getCurrentDate() {
     const today: Date = new Date();
     const dd: string = String(today.getDate()).padStart(2, '0');
     const mm: string = String(today.getMonth() + 1).padStart(2, '0');
     const yy: string = String(today.getFullYear());
     const formattedDate: string = `${dd}-${mm}-${yy}`;
-   return formattedDate
+    return formattedDate
   }
 
 
   login(): void {
     // TODO: Implement login logic
     this.isLoggedIn = true;
-    localStorage.setItem('isLoggedIn','true')
+    localStorage.setItem('isLoggedIn', 'true')
     this.router.navigate(['/tabs/home']);
   }
 
   logout(): void {
     // Clear login status
     this.isLoggedIn = false;
-    localStorage.setItem('isLoggedIn','false')
+    localStorage.setItem('isLoggedIn', 'false')
     // Clear cache and redirect to login page
-     setTimeout(() => {
+    setTimeout(() => {
       this.router.navigateByUrl('/login', { skipLocationChange: true }).then(() => {
         this.router.navigate(['/login']);
-     });
-    },100);
+      });
+    }, 100);
   }
 
   checkLoggedIn(): boolean {
     // return this.isLoggedIn;
-    return localStorage.getItem('isLoggedIn') == 'true'?true:false;
+    return localStorage.getItem('isLoggedIn') == 'true' ? true : false;
   }
+
+
+  // Function to read the Excel file and retrieve headers and data
+  readExcelFile(file: File): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+        resolve(jsonData);
+      };
+
+      reader.onerror = (e) => {
+        reject(e);
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+  // Function to extract headers and their values from the Excel data
+  extractHeadersData(data: any[][]): { header: string, array: any[] }[] {
+    const headers = data[0];
+    const values = data.slice(1);
+    const result = [];
+
+    for (let i = 0; i < headers.length; i++) {
+      result.push({
+        header: headers[i],
+        array: values.map((row) => row[i]),
+      });
+    }
+
+    return result;
+  }
+
+
+
+  async uploadExcelFile(file: File) {
+    let uploadResponse = {status : null,message : ''}
+    let response:any = ""
+    try {
+      if (file) {
+        const excelData = await this.readExcelFile(file);
+        const headerData = this.extractHeadersData(excelData);
+        let body  = { data : headerData}
+        response = await this.postData(environment.nodeApi, body).toPromise();
+  
+        uploadResponse.message = response.status === 200 ? 'File was successfully uploaded' : 'Error reading Excel file';
+      }
+    } catch (error) {
+      uploadResponse.message = 'Error reading Excel file';
+    }
+    return uploadResponse.status = response.status
+  }
+  
+
+  postData(url: string, data: any, headers?: any) {
+    return this.http.post(url, data, { headers: headers })
+  }
+
+  getData(url: string, data: any, headers?: any) {
+    return this.http.get(url, { headers: headers })
+  }
+
+
+
 
 }
