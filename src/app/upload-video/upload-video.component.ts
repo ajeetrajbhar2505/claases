533522f00c7fabc8eacde625c8e429d3b67f58bd
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2023-10-03 22:49:28 +0530

    update pages

--- src/app/upload-video/upload-video.component.ts
+++ src/app/upload-video/upload-video.component.ts
@@ -15,9 +15,9 @@ import { Subject, takeUntil } from 'rxjs';
 })
 export class UploadVideoComponent implements OnInit {
   private _unsubscribeAll: Subject<any>;
-  lecturesData: any[] = []
-  classData: any = []
-  params: any = {}
+  lecturesData: any[] = [];
+  classData: any = [];
+  params: any = {};
   uploadVideogroup = new FormGroup({
     classId: new FormControl(''),
     lec_id: new FormControl(''),
@@ -27,110 +27,111 @@ export class UploadVideoComponent implements OnInit {
     content: new FormControl('video'),
     published_at: new FormControl(''),
     file: new FormControl(''),
-  })
-  currentContent: any = ""
-  uploading: boolean = false
-  uploadStatus: any = { status: false, message: '',statusType: '', }
+  });
+  currentContent: any = '';
+  uploading: boolean = false;
+  uploadStatus: any = { status: false, message: '', statusType: '' };
 
-  constructor(public http: HttpClient, private sanitizer: DomSanitizer, public router: Router, public service: WebService, public ActivatedRoute: ActivatedRoute) {
-    this._unsubscribeAll = new Subject()
-    this.uploadVideogroup.get('published_at')?.patchValue(this.service.getCurrentDate())
+  constructor(
+    public http: HttpClient,
+    private sanitizer: DomSanitizer,
+    public router: Router,
+    public service: WebService,
+    public ActivatedRoute: ActivatedRoute
+  ) {
+    this._unsubscribeAll = new Subject();
+    this.uploadVideogroup
+      .get('published_at')
+      ?.patchValue(this.service.getCurrentDate());
     ActivatedRoute.queryParams.subscribe((params: any) => {
-      this.params = params
-      this.uploadVideogroup.get('content')?.patchValue(params.content)
-    })
+      this.params = params;
+      this.uploadVideogroup.get('content')?.patchValue(params.content);
+    });
   }
 
   async ngOnInit() {
-    this.fetchClassDetails()
+    this.fetchClassDetails();
   }
 
   async fetchClassDetails() {
-    const req = new Requestmodels()
+    const req = new Requestmodels();
     req.RequestUrl = `classDetails`;
-    req.RequestObject = ""
-  
-    await this.service
-     .fetchData(req)
-     .pipe(takeUntil(this._unsubscribeAll))
-     .subscribe(
-      (data) => {
-       if (data != null) {
-        if (data.status !== 200) {
-         return
-        }
-  
-        // fetch
-         this.classData = data.response || []
-       }
-      },
-      (_error) => {
-       return;
-      },
-      () => {
-  
-      }
-  
-     )
-    }
+    req.RequestObject = '';
 
-  readUrl(event: any) {
-    this.uploadVideogroup.get("file")?.patchValue(event.target.files[0]);
-    this.uploadVideogroup.get("content_link")?.patchValue(event.target.files[0].name);
+    await this.service
+      .fetchData(req)
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(
+        (data) => {
+          if (data != null) {
+            if (data.status !== 200) {
+              return;
+            }
+
+            // fetch
+            this.classData = data.response || [];
+          }
+        },
+        (_error) => {
+          return;
+        },
+        () => {}
+      );
   }
 
+  readUrl(event: any) {
+    this.uploadVideogroup.get('file')?.patchValue(event.target.files[0]);
+    this.uploadVideogroup
+      .get('content_link')
+      ?.patchValue(event.target.files[0].name);
+  }
 
   getImgContent(url: any): SafeUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   async getSubjectsByclassId(event: any) {
-    const classId = event.target.value
-    this.fetchlectureDetails(classId)
+    const classId = event.target.value;
+    this.fetchlectureDetails(classId);
   }
 
-  async fetchlectureDetails(classId:any) {
-    const req = new Requestmodels()
+  async fetchlectureDetails(classId: any) {
+    const req = new Requestmodels();
     req.RequestUrl = `lectureDetails/` + classId;
-    req.RequestObject = ""
-  
+    req.RequestObject = '';
+
     await this.service
-     .fetchData(req)
-     .pipe(takeUntil(this._unsubscribeAll))
-     .subscribe(
-      (data) => {
-       if (data != null) {
-        if (data.status !== 200) {
-         return
-        }
-  
-        
-        // fetch
-         this.lecturesData = data.response || []
-       }
-      },
-      (_error) => {
-       return;
-      },
-      () => {
-  
-      }
-  
-     )
-    }
+      .fetchData(req)
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(
+        (data) => {
+          if (data != null) {
+            if (data.status !== 200) {
+              return;
+            }
+
+            // fetch
+            this.lecturesData = data.response || [];
+          }
+        },
+        (_error) => {
+          return;
+        },
+        () => {}
+      );
+  }
 
   switchContent(content: any) {
-    this.currentContent = content
-    this.uploadVideogroup.get('content')?.patchValue(content)
+    this.currentContent = content;
+    this.uploadVideogroup.get('content')?.patchValue(content);
   }
 
   onchange_lecture(event: any) {
-
     this.lecturesData.filter((data: any) => {
-      if (data.lec_id == this.uploadVideogroup.get('lec_id')?.value) {
-        this.uploadVideogroup.get('content_icon')?.patchValue(data.lec_icon)
+      if (data.lec_id == this.uploadVideogroup.get('lec_id')?.value || '') {
+        this.uploadVideogroup.get('content_icon')?.patchValue(data.lec_icon);
       }
-    })
+    });
   }
 
   backToContent() {
@@ -138,52 +139,61 @@ export class UploadVideoComponent implements OnInit {
       classId: this.params.classId,
       lec_id: this.params.lec_id,
       contentId: this.params.contentId,
-      from: '/tabs/lectures'
+      from: '/tabs/lectures',
     };
     this.router.navigate([this.params.from], { queryParams });
   }
 
   async uploadContent() {
+    console.log(this.uploadVideogroup.value);
 
-    this.uploading = true
-    let body: any = { ...this.uploadVideogroup.value }
+    this.uploading = true;
 
     // create a new FormData object
     const formData = new FormData();
-    for (const key in body) {
-      if (body.hasOwnProperty(key)) {
-        formData.append(key, body[key]);
-      }
-    }
 
+    const classId = this.uploadVideogroup.get('classId')?.value || '';
+    const lec_id = this.uploadVideogroup.get('lec_id')?.value || '';
+    const content_icon = this.uploadVideogroup.get('content_icon')?.value || '';
+    const content_link = this.uploadVideogroup.get('content_link')?.value || '';
+    const content_title =
+      this.uploadVideogroup.get('content_title')?.value || '';
+    const content = this.uploadVideogroup.get('content')?.value || '';
+    const published_at = this.uploadVideogroup.get('published_at')?.value || '';
+    const file = this.uploadVideogroup.get('file')?.value || '';
 
-    const req = new Requestmodels()
+    formData.append('classId', classId);
+    formData.append('lec_id', lec_id);
+    formData.append('content_icon', content_icon);
+    formData.append('content_link', content_link);
+    formData.append('content_title', content_title);
+    formData.append('content', content);
+    formData.append('published_at', published_at);
+    formData.append('file', file);
+
+    const req = new Requestmodels();
     req.RequestUrl = `upload`;
-    req.RequestObject = formData
-  
+    req.RequestObject = formData;
+
     await this.service
-     .PostData(req)
-     .pipe(takeUntil(this._unsubscribeAll))
-     .subscribe(
-      (data) => {
-       if (data != null) {
-        if (data.status !== 200) {
-         return
-        }
-  
-        // fetch
-      this.uploading = false
-         
-       }
-      },
-      (_error) => {
-       return;
-      },
-      () => {
-  
-      }
-  
-     )
+      .PostData(req)
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(
+        (data) => {
+          if (data != null) {
+            if (data.status !== 200) {
+              return;
+            }
+
+            // fetch
+            this.uploading = false;
+          }
+        },
+        (_error) => {
+          return;
+        },
+        () => {}
+      );
 
     // formData can now be sent in an HTTP request
     // try {
@@ -194,16 +204,16 @@ export class UploadVideoComponent implements OnInit {
     // } catch (error) {
     //   this.uploading = false
     // }
-
   }
 
   async uploadExcel(event: any) {
-    this.uploadStatus.status = true
-    this.uploadStatus = await this.service.uploadExcelFile(event.target.files[0])
+    this.uploadStatus.status = true;
+    this.uploadStatus = await this.service.uploadExcelFile(
+      event.target.files[0]
+    );
   }
 
-  getSnackbarStatus(status:any){
-    this.uploadStatus.status = status
+  getSnackbarStatus(status: any) {
+    this.uploadStatus.status = status;
   }
-
 }
