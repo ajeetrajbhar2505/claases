4b6a0bae71f4a1f2e786367484f987be07725126
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2023-10-12 02:06:20 +0530

    updated calendarDates

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { WebService } from '../web.service';
 
 @Component({
   selector: 'app-calender',
@@ -8,8 +9,9 @@ import { Router } from '@angular/router';
 })
 export class CalenderComponent {
   currentDay: number = 1;
-  calendarDates: number[][] = [];
+  calendarDates: any[][] = [];
   currentMonth: number;
+  currentMonthName: any;
   currentYear: number;
   transformedCalenderEvents: CalendarEvent[] = [];
   calenderEvents: CalendarEvent[] = [
@@ -18,67 +20,128 @@ export class CalenderComponent {
       time: '14.00',
       month: 'Oct',
       titile: 'Biology Test',
-      date: new Date().toString(),
+      date: this.service.getCurrentDate(),
     },
     {
       day: 5,
       time: '14.00',
       month: 'Oct',
       titile: 'English Test',
-      date: new Date().toString(),
+      date: this.service.getCurrentDate(),
     },
     {
       day: 7,
       time: '14.00',
       month: 'Oct',
       titile: 'History Test',
-      date: new Date().toString(),
+      date: this.service.getCurrentDate(),
     },
     {
       day: 25,
       time: '12.00',
       month: 'Oct',
       titile: 'Zoom Meeting',
-      date: new Date().toString(),
+      date: this.service.getCurrentDate(),
     },
     {
       day: 26,
       time: '16.00',
       month: 'Oct',
       titile: 'Maths Test',
-      date: new Date().toString(),
+      date: this.service.getCurrentDate(),
     },
     {
       day: 27,
       time: '10.00',
       month: 'Oct',
       titile: 'History Test',
-      date: new Date().toString(),
+      date: this.service.getCurrentDate(),
     },
   ];
 
-  constructor(public router: Router) {
+  constructor(public router: Router,public service:WebService) {
     this.currentMonth = new Date().getMonth();
+    this.currentMonthName = new Date().toLocaleString('default', { month: 'long' });
     this.currentYear = new Date().getFullYear();
     this.generateCalendarDates(this.currentYear, this.currentMonth);
   }
 
   generateCalendarDates(year: number, month: number) {
-    // Generate an array of arrays for a month, where each inner array represents a row of dates
+    // Calculate the number of days in the current month
     const daysInMonth = new Date(year, month + 1, 0).getDate();
-    this.calendarDates = this.chunkArray(
-      Array.from({ length: daysInMonth }, (_, i) => i + 1),
-      7
-    );
+  
+    // Get the day of the week for the 1st day of the month (0: Sunday, 1: Monday, ..., 6: Saturday)
+    const firstDayOfWeek = new Date(year, month, 1).getDay();
+  
+    // Calculate the number of days from the previous month to include
+    const daysFromPreviousMonth = firstDayOfWeek;
+  
+    // Calculate the number of days from the next month to include
+    const totalDays = daysInMonth + daysFromPreviousMonth;
+    console.log(totalDays);
+    
+    const daysFromNextMonth = Math.ceil(totalDays / 7) * 7 - totalDays;
+  
+    // Create an array containing days from the previous month
+    const daysArray = [
+      ...Array.from({ length: daysFromPreviousMonth }, (_, i) => daysInMonth - daysFromPreviousMonth + i + 1),
+      ...Array.from({ length: daysInMonth }, (_, i) => i + 1),
+      ...Array.from({ length: daysFromNextMonth }, (_, i) => i + 1),
+    ];
+  
+    // Chunk the array into rows
+    this.calendarDates = this.chunkArray(daysArray, 7);
+    
+  
+    // Set the current day
     this.currentDay = new Date().getDate();
+  
+    
     // Call the function to transform the array
-    this.transformedCalenderEvents = this.transformCalendarEvents(
-      this.calenderEvents
-    );
+    this.transformedCalenderEvents = this.transformCalendarEvents(this.calenderEvents);
+    let count = 1; // Initialize count outside the map function
+    this.calendarDates = this.calendarDates.map(row => {
+      return row.map(date => {
+        const dateObj = new Date(year, month, date);
+        const localday = dateObj.getDate().toString().padStart(2, '0');
+        
+        if (totalDays === date) {
+          count++;
+        }
+        
+        const localmonth = (totalDays === date)?(dateObj.getMonth() + 1).toString().padStart(2, '0'):(dateObj.getMonth() + count).toString().padStart(2, '0');
+        const localyear = dateObj.getFullYear();
+        const formattedDate = `${localday}-${localmonth}-${localyear}`;
+        
+        return {
+          day: date,
+          date: formattedDate
+        };
+      });
+    });
+    
   }
+  
+
+
+  
+
+  // Function to chunk an array into sub-arrays
+  chunkArray(array: number[], size: number): number[][] {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size));
+    }
+    return result;
+  }
+
 
   checkDateExists(date:number):boolean{
     return this.calenderEvents.some(item => item.day === date);
+  }
+
+  checkCurrentMonthExists(date:number):boolean{
+    return true
   }
 
   checkSeriesDateExists(startDate: number, endDate: number): boolean {
@@ -100,16 +163,24 @@ export class CalenderComponent {
     // Check if the current date ends a series
     return this.checkSeriesDateExists(date - 2, date);
   }
-  
 
-  // Function to chunk an array into sub-arrays
-  chunkArray(array: number[], size: number): number[][] {
-    const result = [];
-    for (let i = 0; i < array.length; i += size) {
-      result.push(array.slice(i, i + size));
+  checkLargestDayEndsAndNextDayStarts(endDate: number): boolean {
+    // Check if the largest day in the series ends and the next day starts
+    if (
+      this.calenderEvents.some(item => item.day === endDate) &&
+      !this.calenderEvents.some(item => item.day === endDate + 1)
+    ) {
+      return true;
     }
-    return result;
+    return false;
   }
+
+
+  isPreviousValueGreater(prev: number, current: number): boolean {
+    return prev > current;
+  }
+  
+  
 
   goToPreviousMonth() {
     if (this.currentMonth === 0) {
@@ -118,13 +189,14 @@ export class CalenderComponent {
     } else {
       this.currentMonth--;
     }
+    this.currentMonthName = new Date(this.currentYear, this.currentMonth, 1).toLocaleString('default', { month: 'long' });
     this.generateCalendarDates(this.currentYear, this.currentMonth);
   }
-
+  
   backToHome() {
     this.router.navigate(['/tabs/home']);
   }
-
+  
   goToNextMonth() {
     if (this.currentMonth === 11) {
       this.currentMonth = 0;
@@ -132,6 +204,7 @@ export class CalenderComponent {
     } else {
       this.currentMonth++;
     }
+    this.currentMonthName = new Date(this.currentYear, this.currentMonth, 1).toLocaleString('default', { month: 'long' });
     this.generateCalendarDates(this.currentYear, this.currentMonth);
   }
 
