2b1b351e4e0279bb6eeb94d6e208bf74c2240b74
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2023-10-19 17:01:53 +0530

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apifirstKey : 'https://cdn.glitch.me/77fbbc57-651f-4482-aa3c-97402292b10b/',
   apilastkey : '?v=1677959874652',
-  nodeApi : 'https://married-bloom-snail.glitch.me/'
+  nodeApi : 'http://65.1.106.152:3000/'
 };
 
 /*
