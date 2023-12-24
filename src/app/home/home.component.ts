9552fbbe764c7e52a1ba3ce387b866ad92c1526d
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2023-12-24 20:31:16 +0530

    new updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -398,7 +398,7 @@ export class HomeComponent implements OnInit {
       from: '/tabs/home',
       classId: contentDetails.classId,
       lec_id: contentDetails.lec_id,
-      contentId: contentDetails._id,
+      contentId: contentDetails.contentId,
       content: contentDetails.content,
       reload: 'true',
     }
